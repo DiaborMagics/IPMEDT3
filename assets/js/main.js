d40c374d61b3b1767951f86dd1035a3d84228122
DiaborMagics <s1089491@student.hsleiden.nl>
2016-12-08 19:43:02 +0100

    Menu is helemaal Material voor Desktop; responsive maken isss nextttt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,54 @@
-$(window).resize(function()
+window.onload = function()
 {
-    resize();
-});
+    document.getElementById('navIntroductionPage').style.color = '#d50000';
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+}
+// $(window).resize(function()
+// {
+//     resize();
+// });
+//
+//
+// function resize()
+// {
+//     $("#introduction h1").css("left", (($("#introduction").width() - $("#introduction h1").width()) / 2));
+// }
 
+//functie om huidige pagina link in menu te highlighten en kleuren
+// $(function()
+// {
+//     $('a').each(function()
+//     {
+//         if ($(this).prop('href') == window.location.href)
+//         {
+//             $(this).addClass('huidig');
+//
+//         }
+//     });
+// });
+//
+// function reload()
+// {
+//     location.reload();
+// }
 
-function resize()
+function highlightIntro()
 {
-    $("#introduction h1").css("left", (($("#introduction").width() - $("#introduction h1").width()) / 2));
+    document.getElementById('navIntroductionPage').style.color ='#d50000';
+    document.getElementById('navPokemonPage').style.color ='#212121';
+    document.getElementById('navVRpage').style.color ='#212121';
+}
+
+function highlightPokemon()
+{
+    document.getElementById('navPokemonPage').style.color ='#d50000';
+    document.getElementById('navIntroductionPage').style.color ='#212121';
+    document.getElementById('navVRpage').style.color ='#212121';
+}
+
+function highlightVR()
+{
+    document.getElementById('navVRpage').style.color ='#d50000';
+    document.getElementById('navPokemonPage').style.color ='#212121';
+    document.getElementById('navIntroductionPage').style.color ='#212121';
 }
